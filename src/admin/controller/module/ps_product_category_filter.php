a684b9646f820cdcdff221d44057ac4f7bf29bc6
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2026-03-12 11:34:47 +0100

    Fix breadcrumb URL to extension

--- src/admin/controller/module/ps_product_category_filter.php
+++ src/admin/controller/module/ps_product_category_filter.php
@@ -40,7 +40,7 @@ class PsProductCategoryFilter extends \Opencart\System\Engine\Controller
 
         $data['breadcrumbs'][] = [
             'text' => $this->language->get('heading_title'),
-            'href' => $this->url->link('extension/module/ps_product_category_filter', 'user_token=' . $this->session->data['user_token'], true),
+            'href' => $this->url->link('extension/ps_product_category_filter/module/ps_product_category_filter', 'user_token=' . $this->session->data['user_token'], true),
         ];
 
 
